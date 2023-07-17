f76dff1468fc657c956ed731a5e00f0c10e3e61a
Olivia Stephens <niceygy@niceygylive.xyz>
2023-07-17 11:38:27 +0100

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,3 +1,3 @@
-curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-arm64' --output vscode_cli.tar.gz
-
+echo curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-arm64' --output vscode_cli.tar.gz
+wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-armhf
 tar -xf vscode_cli.tar.gz
